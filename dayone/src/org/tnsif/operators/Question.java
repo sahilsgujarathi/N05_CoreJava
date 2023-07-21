e9e96052538626a6c21f2f47b50100135ae948dc
Sahil Gujarathi <Sahil Gujarathi@DESKTOP-JHL34ML>
2023-07-21 11:57:48 +0530

    Second Commit

---
+++ dayone/src/org/tnsif/operators/Question.java
@@ -0,0 +1,26 @@
+// Program to demonstrate that the Person is Eligible for Blood Donation or Not.
+package org.tnsif.operators;
+import java.util.Scanner;
+
+public class Question {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter Your Age:- ");
+		int age = sc.nextInt();
+		System.out.println("Enter Your Weight:- ");
+		float weight = sc.nextFloat();
+		
+		if((age>=18) && (weight>=50.00f)) {
+			System.out.println("You are Eligible for Blood Donation.");
+		}
+		else {
+			System.out.println("You are not Eligible for Blood Donation");
+		}
+		
+		sc.close();
+	}
+
+}
